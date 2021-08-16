c1127e91959754981e5956177bf0da5a674d2ba9
KS <kn.shiryaev@gmail.com>
2021-08-16 20:31:48 +0500

    Add save of content webpage

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,19 +1,45 @@
 import requests
+import sys
 
 from bs4 import BeautifulSoup as BS
 
 MOVIE = {}
-url = input("Input your URL:").strip()
+URL = input("Input your URL:").strip()
 
-request = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
 
-if "imdb" and "title" in url:
-    if request.status_code == 200:
-        soup = BS(request.content, 'html.parser')
-        MOVIE = {'title': soup.find("h1").text,
-                 'description': soup.select_one("[data-testid='plot-l']").text}
-        print(MOVIE)
+def get_url():
+    response = requests.get(URL, headers={'Accept-Language': 'en-US,en;q=0.5'})
+    if response.status_code == 200:
+        return response
+    else:
+        # sys.exit(f"The URL returned {request.status_code}!")
+        print(f"The URL returned {response.status_code}!")
+        sys.exit()
+
+
+def save_content(response):
+    try:
+        file = open('source.html', 'wb')
+        file.write(response.content)
+        file.close()
+        print("Content saved.")
+    except SystemError:
+        print("Oops, something wrong")
+
+
+def find_movie(response):
+    save_content(response)
+    if "imdb" and "title" in URL:
+        if response.status_code == 200:
+            soup = BS(response.content, 'html.parser')
+            MOVIE = {'title': soup.find("h1").text,
+                     'description': soup.select_one("[data-testid='plot-l']").text}
+            print(MOVIE)
+        else:
+            print("Invalid movie page!")
     else:
         print("Invalid movie page!")
-else:
-    print("Invalid movie page!")
+
+
+if __name__ == '__main__':
+    find_movie(get_url())
